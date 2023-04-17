f682c7365a2136ec817ee4f67b2a369cfe3646ab
Gulherme Bispo e Gabriel <guilherme19.b07@gmail.com>
2023-04-17 09:35:59 -0300

    Desenvolvimento dos Services

---
+++ AnimesFavoritos/Services/AnimeService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimesFavoritos.Services
+{
+    public class AnimeService
+    {
+        
+    }
+}
